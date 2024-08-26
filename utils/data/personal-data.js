e632deca684b0c1e6e685cc17f8e7f5f06310dae
BigFlag <ilyes.huus@gmail.com>
2024-08-26 23:05:23 +0200

    [ADD] setup data

---
+++ utils/data/personal-data.js
@@ -0,0 +1,15 @@
+export const personalData = {
+  name: "Ilyes",
+  profile: '',
+  designation: "Software Developer",
+  description: "My name is Ilyes. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'ilyes.huus@gmail.com',
+  phone: '+33603611812',
+  address: 'Versailles, France',
+  github: 'https://github.com/Ilyes-Z',
+  linkedIn: 'https://www.linkedin.com/in/ilyes-zeddam/',
+  // twitter: 'https://twitter.com/said7388',
+  // leetcode: "https://leetcode.com/said3812/",
+  devUsername: "Ilyes-Z",
+  resume: ""
+}
